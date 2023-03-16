a5d89c8f62c59742072fe21c9a5e9d10ffa89d2b
igor <igornotfound@gmail.com>
2023-03-16 20:36:28 +0100

    first component, props types / interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Card from "./components/Card";
 
 const App = () => {
     return (
         <div>
-          <h1>React App!</h1>
+          <Card width="200px" height="200px" >
+              <button>Button!</button>
+          </Card>
         </div>
     );
 }
