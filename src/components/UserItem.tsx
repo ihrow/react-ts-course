341cac9fe44338066cf937be088984be505049d4
igor <igornotfound@gmail.com>
2023-03-16 22:03:56 +0100

    decomposition

---
+++ src/components/UserItem.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from 'react';
+import {IUser} from "../types/types";
+
+interface UserItemProps {
+  user: IUser;
+}
+
+const UserItem: FC<UserItemProps> = ({user}) => {
+  return (
+    <div style={{padding: 15, border: '1px solid green'}}>
+      {user.id}. {user.name} lives in {user.address.city} on {user.address.street} street.
+    </div>
+  );
+};
+
+export default UserItem
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react';
 import {IUser} from "../types/types";
+import UserItem from "./UserItem";
 
 interface UserListProps {
   users: IUser[];
@@ -8,9 +9,7 @@ const UserList: FC<UserListProps> = ({users}) => {
   return (
     <div>
       {users.map(user =>
-        <div key={user.id} style={{padding: 15, border: '1px solid green'}}>
-          {user.id}. {user.name} lives in {user.address.city} on {user.address.street} street.
-        </div>
+        <UserItem key={user.id} user={user} />
       )}
     </div>
   );
